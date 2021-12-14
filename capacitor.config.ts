791a28f93d32e3b305b1b50a11e4c197fc7da700
ivanmeotto <ivan.meotto@glue-labs.com>
2021-12-14 15:02:26 +0100

    added android build

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.teuliex.app',
+  appName: 'Teuliex',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
