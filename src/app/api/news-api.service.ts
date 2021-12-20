711ab9b70415c55571a81359c62b0979d01d2942
ivanmeotto <ivan.meotto@glue-labs.com>
2021-12-20 09:30:44 +0100

    fix: fixed login only news

--- src/app/api/news-api.service.ts
+++ src/app/api/news-api.service.ts
@@ -4,18 +4,18 @@ import { Injectable } from '@angular/core';
 import { API_BASE, OLD_API_BASE } from '../shared/constants/consts';
 
 @Injectable({
-	providedIn: 'root',
+  providedIn: 'root',
 })
 export class NewsApiService {
-	constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-	getAllNews(page: number) {
-		const today = new Date();
-		return this.http.get(`${OLD_API_BASE}/news?sortBy=dataPubblicazione&page=${page}&limit=10`).toPromise();
-		//return this.http.get(`${API_BASE}/news?date_after=${formatDate(today, 'yyyy-MM-dd', 'en-US')}`).toPromise();
-	}
+  getAllNews(page: number) {
+    const today = new Date();
+    return this.http.get(`${OLD_API_BASE}/news?sortBy=dataPubblicazione&page=${page}&limit=10`).toPromise();
+    //return this.http.get(`${API_BASE}/news?date_after=${formatDate(today, 'yyyy-MM-dd', 'en-US')}`).toPromise();
+  }
 
-	getOneNews(id: string) {
-		return this.http.get(`${OLD_API_BASE}/news?newsId=${id}`).toPromise();
-	}
+  getOneNews(id: string) {
+    return this.http.get(`${OLD_API_BASE}/news?newsId=${id}`).toPromise();
+  }
 }
--- src/app/pages/private/home/home.page.ts
+++ src/app/pages/private/home/home.page.ts
@@ -8,84 +8,84 @@ import { User } from 'src/app/shared/interfaces/user';
 import { EventRegistrationModalComponent } from '../../../shared/components/event-registration-modal/event-registration-modal.component';
 
 @Component({
-	selector: 'app-home',
-	templateUrl: './home.page.html',
-	styleUrls: ['./home.page.scss'],
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
-	user: User = new User();
-	@ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
-	news: News[] = new Array<News>();
-	defaultImage: string;
-	loading: HTMLIonLoadingElement;
-	pageNumber: number = 1;
-	userIsLogged: boolean = false;
+  user: User = new User();
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+  news: News[] = new Array<News>();
+  defaultImage: string;
+  loading: HTMLIonLoadingElement;
+  pageNumber: number = 1;
+  userIsLogged: boolean = false;
 
-	constructor(
-		private router: Router,
-		private newsService: NewsService,
-		private loadingCtrl: LoadingController,
-		private behaviorsService: BehaviorsService,
-		private modalController: ModalController
-	) {
-		this.defaultImage = './../../../../assets/images/no_image.jpeg';
-		behaviorsService.user.subscribe((userValue) => {
-			this.user = userValue;
-			if (this.user.id) this.userIsLogged = true;
-			else this.userIsLogged = false;
-		});
-	}
+  constructor(
+    private router: Router,
+    private newsService: NewsService,
+    private loadingCtrl: LoadingController,
+    private behaviorsService: BehaviorsService,
+    private modalController: ModalController
+  ) {
+    this.defaultImage = './../../../../assets/images/no_image.jpeg';
+    this.behaviorsService.user.subscribe((userValue) => {
+      this.user = userValue;
+      if (this.user.id) this.userIsLogged = true;
+      else this.userIsLogged = false;
+    });
+  }
 
-	async ionViewWillEnter() {
-		this.loading = await this.loadingCtrl.create({
-			message: 'Retrieving news. Please wait...',
-		});
-		await this.loading.present();
-		this.news = await this.newsService.getAllNews(this.pageNumber);
-		this.news = this.filterNews(this.news);
-		await this.loading.dismiss();
-	}
+  async ionViewWillEnter() {
+    this.loading = await this.loadingCtrl.create({
+      message: 'Retrieving news. Please wait...',
+    });
+    await this.loading.present();
+    this.news = await this.newsService.getAllNews(this.pageNumber);
+    this.news = this.filterNews(this.news);
+    await this.loading.dismiss();
+  }
 
-	openNews(id: string) {
-		this.router.navigate(['private', 'news', id]);
-	}
+  openNews(id: string) {
+    this.router.navigate(['private', 'news', id]);
+  }
 
-	async dismissLoading() {
-		await this.loading.dismiss();
-	}
+  async dismissLoading() {
+    await this.loading.dismiss();
+  }
 
-	async retrieveMoreNews(event: any) {
-		this.pageNumber++;
-		let moreNews = await this.newsService.getAllNews(this.pageNumber);
-		moreNews = this.filterNews(moreNews);
-		if (moreNews.length > 0) {
-			this.news = this.news.concat(moreNews);
-			event.target.complete();
-		} else {
-			this.infiniteScroll.disabled = true;
-		}
-	}
+  async retrieveMoreNews(event: any) {
+    this.pageNumber++;
+    let moreNews = await this.newsService.getAllNews(this.pageNumber);
+    moreNews = this.filterNews(moreNews);
+    if (moreNews.length > 0) {
+      this.news = this.news.concat(moreNews);
+      event.target.complete();
+    } else {
+      this.infiniteScroll.disabled = true;
+    }
+  }
 
-	filterNews(news: News[]) {
-		let tempNews: News[] = [];
-		if (!this.userIsLogged) {
-			news.forEach((notizia) => {
-				if (!notizia.needsLogin) tempNews.push(notizia);
-			});
-		} else {
-			tempNews = news;
-		}
-		return tempNews;
-	}
+  filterNews(news: News[]) {
+    let tempNews: News[] = [];
+    if (!this.userIsLogged) {
+      news.forEach((notizia) => {
+        if (!notizia.needsLogin) tempNews.push(notizia);
+      });
+    } else {
+      tempNews = news;
+    }
+    return tempNews;
+  }
 
-	async openEventRegistration(news: News) {
-		const modal = await this.modalController.create({
-			component: EventRegistrationModalComponent,
-			componentProps: {
-				event: news,
-			},
-			cssClass: 'half-modal',
-		});
-		await modal.present();
-	}
+  async openEventRegistration(news: News) {
+    const modal = await this.modalController.create({
+      component: EventRegistrationModalComponent,
+      componentProps: {
+        event: news,
+      },
+      cssClass: 'half-modal',
+    });
+    await modal.present();
+  }
 }
